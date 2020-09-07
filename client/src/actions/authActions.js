f9714725eccca241fa03f7eb052f53e98f4d64a3
Ryan Huculak <ryanhuculak@yahoo.com>
2020-09-06 23:29:25 -0700

    coded authActions and authReducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,3 +1,70 @@
 import { PLACEHOLDER_TYPE } from './types';
 import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
+import { FloatingActionButton } from 'materialize-css';
+
+// Load user
+export const loadUser = async () => {
+    if(localStorage.token) {
+        setAuthToken(localStorage.token);
+    }
+
+    try {
+        const res = await axios.get('/api/auth'); // todo: make sure default route paths are set up properly
+        dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (err) {
+        dispatch({ type: AUTH_ERROR });
+    }
+}
+
+// Register User
+export const register = async (formData) => {
+    const config = {
+        headers: {
+            'Content-Type:': 'application/json'
+        }
+    }
+    try {
+        const res = await axios.post('/api/users', formData, config);
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        })
+        loadUser();
+    } catch (err) {
+        dispatch({
+            type: REGISTER_FAIL,
+            payload: err.response.data.msg
+        })
+    }
+}
+
+// Login user
+export const login = async (formData) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    try {
+        const res = await axios.post('/api/auth', formData, config);
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+        loadUser();
+    } catch (err) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: err.response.data.msg
+        })
+    }
+}
+
+// Logout
+export const logout = () => dispatch({ type: LOGOUT })
+
+// Clear Errors
+export const clearErrors = () => dispatch({
+    type: CLEAR_ERRORS 
+});
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,13 @@
-import { PLACEHOLDER_TYPE } from '../actions/types';
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT,
+    CLEAR_ERRORS
+} from '../types'
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -10,10 +19,41 @@ const initialState = {
 
 export default (state = initialState, action) => {
     switch(action.type) {
-        case PLACEHOLDER_TYPE:
+        case USER_LOADED:
             return {
-                ...state
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: action.payload
             }
-            default: return state;
+        case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', action.payload.token);
+            return {
+                ...state,
+                ...action.payload,
+                isAuthenticated: true,
+                loading: false
+            }
+        case REGISTER_FAIL:
+        case LOGIN_FAIL:
+        case AUTH_ERROR:
+        case LOGOUT:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false,
+                user: null,
+                error: action.payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                errors: null
+            }
+        default: 
+            return state;
     }
 }
