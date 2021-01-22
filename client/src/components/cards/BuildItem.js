2a213b985d3798e8cbd86783f32f0b3c44490e43
Ryan Huculak <ryanhuculak@yahoo.com>
2021-01-21 22:49:18 -0800

    t

--- client/src/components/cards/BuildItem.js
+++ client/src/components/cards/BuildItem.js
@@ -59,7 +59,47 @@ export const BuildItem = ( { build } ) => {
                         {status}
 
                 </div>
-                <div class="collapsible-body" style={{ backgroundColor: 'white', opacity: '.7'}}><span>Lorem ipsum dolor sit amet.</span></div>
+                <div class="collapsible-body" style={{ backgroundColor: 'white', opacity: '.7'}}># 
+                        {build_number}
+                    
+                        <i 
+                            class="material-icons tooltipped" 
+                            data-position="top" 
+                            data-tooltip="Project Name"
+                            style={{marginLeft: '30px'}}
+                        > build </i>
+                        {projectsString}
+
+                        <i  
+                            class="material-icons tooltipped"
+                            data-position="top" 
+                            data-tooltip="Build Filename"
+                            style={{marginLeft: '30px'}}
+                        > insert_drive_file </i> 
+                        {buildFileName}  
+        
+                        <i 
+                            class="material-icons tooltipped" 
+                            data-position="top" 
+                            data-tooltip="Date Started"
+                            style={{marginLeft: '30px'}}
+                        > event </i> 
+                        {dateStarted.split('T')[0]}
+
+                        {status === 'Build Delivered' ? 
+                                <i class="material-icons tooltipped" 
+                                    data-position="top" 
+                                    data-tooltip="Status"
+                                    style={{marginLeft: '30px'}}
+                                >done</i> 
+                            : 
+                                <i class="material-icons tooltipped" 
+                                    data-position="top" 
+                                    data-tooltip="Status"
+                                    style={{marginLeft: '30px'}}
+                                >hourglass_full</i>
+                        }
+                        {status}</div>
                 </li>
             </ul>
         </div>
