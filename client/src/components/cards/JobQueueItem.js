ab974c5d9d75669280d23535601fd2ac5361f683
Ryan Huculak <ryanhuculak@yahoo.com>
2021-03-11 00:49:11 -0800

    temp commit

--- client/src/components/cards/JobQueueItem.js
+++ client/src/components/cards/JobQueueItem.js
@@ -1,18 +1,30 @@
 import React from 'react'
 
 
-export const JobQueueItem = () => {
+export const JobQueueItem = ({ job }) => {
     return (
         <div className="card" style={{ backgroundColor: '#323840' }}>
-              <div className="card-content white-text">
-                <span className="card-title">Card Title</span>
+              <div className="card-content white-text" style={{ padding: 0 }}>
+                <div className="row">
+                  <div className="col s1 red darken-4" style={{ padding: 2 }}>
+                    #9999
+                  </div>
+                  <div className="col s2">
+                    AIM
+                  </div>
+                  <div className="col s6">
+                    Requested By John on 3/5/2021
+                  </div>
+                  <div className="col s2">
+                    STATUS: Requested
+                  </div>
+
+                </div>
+                <span>Card Title</span>
                 <p>I am a very simple card. I am good at containing small bits of information.
                 I am convenient because I require little markup to use effectively.</p>
               </div>
-              <div className="card-action">
-                <a href="#" className="blue-text">This is a link</a>
-                <a href="#" className="blue-text">This is a link</a>
-              </div>
+              
         </div>
     )
 }
