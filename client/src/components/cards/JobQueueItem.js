38a8b9bfdf9783fc85a04171d9f604e7d120f23e
Ryan Huculak <rhuculak@altaviz.local>
2021-03-11 18:38:55 -0800

    Job queue WIP 3

--- client/src/components/cards/JobQueueItem.js
+++ client/src/components/cards/JobQueueItem.js
@@ -6,38 +6,79 @@ export const JobQueueItem = ({ job }) => {
     return (
         <div className="card" style={{ backgroundColor: '#323840' }}>
               <div className="card-content white-text" style={{ padding: 0 }}>
-                <div className="row">
-                  <div className="col s1 red darken-4" style={{ padding: 2 }}>
+                <div className="row grey darken-3" style={{ margin: 0 }}>
+                  <div className="col s1 grey darken-2" style={{ padding: 2 }}>
                     #{job_number}
                   </div>
                   <div className="col s2">
                     {projectName}
                   </div>
                   <div className="col s6">
-                    Requested By {requester} on 3/5/2021
+                    Requested By {requester} on {dateRequested.split('T')[0]}
                   </div>
-                  <div className="col s3">
+                  <div className="col s3 grey darken-2">
                     STATUS: {status}
                   </div>
                 </div>
-                <div className="row">
-                  <div className="col s3 yellow"></div>
-                  <div className="col s5 align-left red">Part</div>
-                  <div className="col s1 align-right blue">Qty</div>
-                  <div className="col s3 green"></div>
-                </div>
-                {requestedParts.map((partEntry) => { return ( 
-                  <div className="row">
-                    <div className="col s3 yellow"></div>
-                    <div className="col s5 align-left red">{partEntry.name}</div>
-                    <div className="col s1 align-right blue">{partEntry.quantity}</div>
-                    <div className="col s3 green"></div>
+
+                <div className="row grey darken-3">
+                  <div className="col s1" style={{ padding: 2 }}>
+                    
                   </div>
-                )})}
+                  <div className="col s2">
+                    Folder:
+                  </div>
+                  <div className="col s6">
+                    {folderLocation}
+                  </div>
+                  <div className="col s3 grey darken-2">
+                    PRIORITY: {priority}
+                  </div>
+                </div>
+
+                <div className="row" style={{ margin: 0 }}>
+                  <div className="col s3"></div>
+                  <div className="col s5 align-left blue darken-4">Part</div>
+                  <div className="col s1 align-right cyan darken-4">Qty</div>
+                  <div className="col s3"></div>
+                </div>
+                {requestedParts.map((partEntry, index) => { return index % 2 ? ( 
+                    <div className="row" key={index} style={{ margin: 0 }}>
+                      <div className="col s3"></div>
+                      <div className="col s5 align-left blue darken-3">{partEntry.name}</div>
+                      <div className="col s1 align-right cyan darken-3">{partEntry.quantity}</div>
+                      <div className="col s3"></div>
+                    </div>
+                  ) : (
+                    <div className="row" key={index} style={{ margin: 0 }}>
+                      <div className="col s3"></div>
+                      <div className="col s5 align-left blue darken-1">{partEntry.name}</div>
+                      <div className="col s1 align-right cyan darken-1">{partEntry.quantity}</div>
+                      <div className="col s3"></div>
+                    </div>
+                  )
+                })}
+
+                <div className="row"></div>
                 
-              
-                <span>Card Title</span>
-                <p>notes: {notes}</p>
+                <div className="row grey darken-3">
+                  <div className="col s1" style={{ padding: 2 }}>
+                    
+                  </div>
+                  <div className="col s3">
+                    Material: {material}
+                  </div>
+                  <div className="col s3 grey darken-2">
+                    Resolution: {resolution}
+                  </div>
+                  <div className="col s3">
+                    Needed by: {dateNeeded.split('T')[0]}
+                  </div>
+                </div>
+                
+                <div className="row center">
+                  notes: {notes}
+                </div>
               </div>
               
         </div>
