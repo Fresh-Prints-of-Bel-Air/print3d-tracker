fcb60b2aae305bb532ca5a9d6e80d20521e80e51
Daniel Sanchez <dsanchez519@csu.fullerton.edu>
2021-05-11 23:31:45 -0700

    added buildHistory resizing for additional screen resolutions

--- client/src/components/pages/BuildHistory.js
+++ client/src/components/pages/BuildHistory.js
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import BuildList from '../cards/BuildList';
 import BuildSearch from '../cards/BuildSearch';
 
 
 const BuildHistory = () => {
 
+  const [formDimensions, setFormDimensions] = useState({
+    height: '20vh',
+    formMarginBtm: '5.5vh',
+    buttonHeight: '4vh',
+    buttonWidth: '7vw',
+    listSize: '73.2vh'
+
+  });
+
+  //maxHeight of Navbar is 6.8vh
+  //height of Navbar for 1080p is about 6.8vh
+  //height of Navbar for 4k is about 5vh
+
+  useEffect(() => {
+    if(window.screen.height * window.devicePixelRatio > 1080)
+      setFormDimensions({...formDimensions, height: '16vh', formMarginBtm: '3vh', listSize: '79vh'});
+  }, []);
 
   return (
     <div>
-      <BuildSearch></BuildSearch>
-      <BuildList></BuildList>
+      <BuildSearch formDimensions={formDimensions}></BuildSearch>
+      <div name='dummydiv' style={{width: '100%', height: formDimensions.height }}></div>
+      {/*dummy div is to add space below the build search, which is a fixed div */}
+      <BuildList formDimensions={formDimensions}></BuildList>
     </div>
   )
 }
