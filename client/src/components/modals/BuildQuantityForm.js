006b45345c081c7e9e25c2d933759389c2a807c9
Daniel Sanchez <dsanchez519@csu.fullerton.edu>
2021-05-15 00:30:24 -0700

    small adjustments for BuildModal

--- client/src/components/modals/BuildQuantityForm.js
+++ client/src/components/modals/BuildQuantityForm.js
@@ -16,8 +16,10 @@ const BuildQuantityForm = ({job, handleQuantityChange}) => {
       <div className="row">Parts for job# {job.job_number} from requester {job.requester}: </div>
       {job.requestedParts.filter((part) => part.remaining > 0).map((part, index) => 
           <div className="row" id={index}>
-            <label htmlFor="partQuantity">{`Quantity for ${part.name} (${part.remaining} remaining): `}</label>
-            <input className="input-field" type="number" name={part.name} id="partQuantity" jobID={job._id} min="0" max={part.remaining} onChange={onChange}/> {/*add value field? */}
+            <div className="col s6 offset-s3">
+              <label htmlFor="partQuantity">{`Quantity for ${part.name} (${part.remaining} remaining): `}</label>
+            </div>
+            
           </div>
         )
       }
