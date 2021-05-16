fc803a4e80952fc76ab7df63fce7c50a3096ebf0
Daniel Sanchez <dsanchez519@csu.fullerton.edu>
2021-05-15 23:08:48 -0700

    Changed BuildQuantityForm to use a React-Select

--- client/src/components/modals/BuildQuantityForm.js
+++ client/src/components/modals/BuildQuantityForm.js
@@ -1,29 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Fragment } from 'react';
+import Select from 'react-select';
 
 const BuildQuantityForm = ({job, handleQuantityChange}) => {
   const [partQuantities, setPartQuantities] = useState(job.requestedParts.map((job) => 0));
   
-  const onChange = (e) => {
-    let quantityChange = e.target.value - partQuantities[e.target.id]; // new value minus old
+  const onChange = (option, index, partName) => {
+    console.log(option);
+    console.log(index);
+    let quantityChange = option.value - partQuantities[index]; // new value minus old
     let quantityArr = [...partQuantities];
-    quantityArr[e.target.id] = e.target.value;
+    console.log("QuantityArr");
+    console.log(quantityArr);
+    quantityArr[index] = option.value;
     setPartQuantities(quantityArr);
     // if(isIncluded) 
-    handleQuantityChange(job._id, e.target.name, quantityChange);
+    handleQuantityChange(job._id, partName, quantityChange);
   }
   return (
-    <div className="row" style={{overflow: 'auto', height: '20vh'}}>
-      <div className="row">Parts for job# {job.job_number} from requester {job.requester}: </div>
+    <Fragment>
+    <div className="row">Parts for job# {job.job_number} from requester {job.requester}: </div>
+    <div className="row" style={{overflow: 'auto', height: '22vh'}}>
       {job.requestedParts.filter((part) => part.remaining > 0).map((part, index) => 
           <div className="row" id={index}>
-            <div className="col s6 offset-s3">
+            <div className="col s3">
               <label htmlFor="partQuantity">{`Quantity for ${part.name} (${part.remaining} remaining): `}</label>
             </div>
-            
+            <div className="col s3 offset-s2">
+              <Select
+                options={[...Array(part.remaining * 2).keys()].map((value) => ({value: value.toString(), label: value.toString()}))}
+                onChange={(option) => onChange(option, index, part.name)}
+              />
+            </div>
           </div>
         )
       }
     </div>
+    </Fragment>
+    
   )
 }
 
