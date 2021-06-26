4ab19f92ec8073605e67f54d54d90e8333c640ec
Ryan Huculak <ryanhuculak@yahoo.com>
2021-06-26 03:47:01 -0700

    alerts for login/register pages form validation

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth');
 const Admin = require('../models/Admin');
 const AdminList = require('../models/AdminList');
 const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
 router.get('/', 
     auth, 
@@ -46,61 +47,74 @@ router.put('/pull', auth, async (req, res) => {
     }
 })
 
-router.put('/register', async (req, res) => { //Handles registration requests
+//Handles registration requests
+// occurs when somebody completes the register form to submit a registration requests
+router.put('/register', async (req, res) => { 
     try {
         console.log("admin put / route");
         console.log(req.body);
-        const adminInfo = await Admin.find({}); //check if the registration request exists already
 
-        let userRequestAlreadyExists = false;
-        console.log("ADMIN INFO");
-        console.log(adminInfo);
-        adminInfo[0].registrationRequests.forEach((regReq) => {
-          if (regReq.email === req.body.email){
-            userRequestAlreadyExists = true;
-          }
-        });
-
-        if(!userRequestAlreadyExists){ //create the registration request if one with that email doesn't already exist
-            // generates salt for hashing function
-        const salt = await bcrypt.genSalt(10);
-        // password is saved only as a hash code
-        let hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-        const regRequest = {
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
+        if (req.body.password.length < 6){
+            res.status(400).send('Please enter a password with a length of six characters or more');
         }
-
-        //ex 12/28/2020, need to change to 2020-12-18
-        let today = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles'}).split(',')[0]; 
-        today = today.split('/');
-        today = today[2] + '-' + today[0] + '-' + today[1]; //2020-12-18
         
-        let registrationRequestNotification = {
-            text: `You have received a new registration request from ${req.body.name} (${req.body.email}).`,
-            dateCreated: today,
-            isRead: false,
-        }
-      
-        // console.log(updateFields);
-        let updatedAdmin = await Admin.findByIdAndUpdate(
-            { _id: '60c731de187465d31a399ca4' }, 
-            { $push: 
-                { 
-                    registrationRequests: { $each: [regRequest], $position: 0 }, 
-                    notifications: { $each: [registrationRequestNotification], $position: 0 }
+        userWithThisEmail = await User.findOne({ email: req.body.email });
+
+        if (!userWithThisEmail){
+            // check if registration request for this user already exists
+            const adminInfo = await Admin.find({});
+            let userRequestAlreadyExists = false;
+            console.log("ADMIN INFO");
+            console.log(adminInfo);
+            adminInfo[0].registrationRequests.forEach((regReq) => {
+            if (regReq.email === req.body.email){
+                userRequestAlreadyExists = true;
+            }
+            });
+
+            //create the registration request if one with that email doesn't already exist
+            if(!userRequestAlreadyExists){ 
+                // generates salt for hashing function
+                const salt = await bcrypt.genSalt(10);
+                // password is saved only as a hash code
+                let hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+                const regRequest = {
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashedPassword
                 }
-            },
-            { new: true },
-        );
-        res.json({message: 'Request received.'});
-        console.log("mongooseReturnVal");
-        console.log(res);
-        }
-        else {
-            res.status(500).send('A registration request using that email already exists.');
+
+                //ex 12/28/2020, need to change to 2020-12-18
+                let today = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles'}).split(',')[0]; 
+                today = today.split('/');
+                today = today[2] + '-' + today[0] + '-' + today[1]; //2020-12-18
+            
+                let registrationRequestNotification = {
+                    text: `You have received a new registration request from ${req.body.name} (${req.body.email}).`,
+                    dateCreated: today,
+                    isRead: false,
+                }
+
+                let updatedAdmin = await Admin.findByIdAndUpdate(
+                    { _id: '60c731de187465d31a399ca4' }, 
+                    { $push: 
+                        { 
+                            registrationRequests: { $each: [regRequest], $position: 0 }, 
+                            notifications: { $each: [registrationRequestNotification], $position: 0 }
+                        }
+                    },
+                    { new: true },
+                );
+                res.json({message: 'Request received.'});
+                console.log("mongooseReturnVal");
+                console.log(res);
+            
+            } else {
+                res.status(400).send('A registration request using that email already exists.');
+            }
+        } else {
+            res.status(400).send('A user with that email already exists.');
         }
     } catch (error) {
         console.log(error.message);
