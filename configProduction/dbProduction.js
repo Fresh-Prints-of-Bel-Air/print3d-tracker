78175f5dde403d99b509ae4f815ec535623bc18d
Daniel Sanchez <dsanchez519@csu.fullerton.edu>
2021-07-02 03:25:33 -0700

    env changes

--- configProduction/dbProduction.js
+++ configProduction/dbProduction.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV !== 'production') {  
+    require('dotenv').config();
+}
+
 const db = process.env.mongoURI;
 
 const connectDBProduction = async () => {
